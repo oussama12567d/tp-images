439f536625227d08b2144c98e758fa777f0a3326
AbdelghaniDjedidi2001 <abdelghanidjedidi7@gmail.com>
2024-05-12 17:39:02 +0100

    6 commit

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ from typing import Union
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import function as func
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
